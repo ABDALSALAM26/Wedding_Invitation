17bcc303120a678c36c23921d4013509201afcc2
ABDALSALAM26 <mohamedahmedabdalsalam2006@gmail.com>
2026-07-19 18:18:34 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,141 +1,123 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-    });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
 });
 
-document.querySelectorAll(".hidden").forEach(el => {
-    observer.observe(el);
+document.querySelectorAll(".hidden").forEach((el) => {
+  observer.observe(el);
 });
 
 const weddingDate = new Date("August 4, 2026 00:00:00");
 const endWeddingDay = new Date("August 5, 2026 00:00:00");
 
 const countdown = setInterval(() => {
-    const now = new Date();
+  const now = new Date();
 
-    if (now >= weddingDate && now < endWeddingDay) {
-        document.getElementById("countdown").style.display = "none";
-        document.getElementById("date-box").style.display = "none";
-        document.getElementById("title").style.display = "none";
+  if (now >= weddingDate && now < endWeddingDay) {
+    document.getElementById("countdown").style.display = "none";
+    document.getElementById("date-box").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("special-message").innerHTML = "𝓣𝓱𝓲𝓼 𝓓𝓪𝔂❤️";
+    return;
+  }
 
-        document.getElementById("special-message").innerHTML = "𝓣𝓱𝓲𝓼 𝓓𝓪𝔂❤️";
-        return;
-    }
+  if (now >= endWeddingDay) {
+    clearInterval(countdown);
 
-    if (now >= endWeddingDay) {
-        clearInterval(countdown);
+    document.getElementById("countdown").style.display = "none";
+    document.getElementById("date-box").style.display = "flex";
 
-        document.getElementById("countdown").style.display = "none";
-        document.getElementById("date-box").style.display = "flex";
+    document.getElementById("title").innerHTML = "Wedding Date";
 
-        document.getElementById("title").innerHTML = "Wedding Date";
+    document.getElementById("d-day").innerHTML = "04";
+    document.getElementById("d-month").innerHTML = "08";
+    document.getElementById("d-year").innerHTML = "2026";
 
-        document.getElementById("d-day").innerHTML = "04";
-        document.getElementById("d-month").innerHTML = "08";
-        document.getElementById("d-year").innerHTML = "2026";
+    return;
+  }
 
-        return;
-    }
+  const distance = weddingDate - now;
 
-    const distance = weddingDate - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
 }, 1000);
-
-
-
-
-
-
 
 const locationIcon = document.querySelector(".location-icon");
 const locationIcon2 = document.querySelector(".location-icon2");
 
-if(sessionStorage.getItem("hannaOpened")){
-    locationIcon.classList.add("location-opened");
+if (sessionStorage.getItem("hannaOpened")) {
+  locationIcon.classList.add("location-opened");
 }
 
-if(sessionStorage.getItem("weddingOpened")){
-    locationIcon2.classList.add("location-opened");
+if (sessionStorage.getItem("weddingOpened")) {
+  locationIcon2.classList.add("location-opened");
 }
 
 if (locationIcon) {
-    locationIcon.addEventListener("click", () => {
+  locationIcon.addEventListener("click", () => {
+    sessionStorage.setItem("hannaOpened", "true");
+    locationIcon.classList.add("location-opened");
 
-        sessionStorage.setItem("hannaOpened", "true");
-        locationIcon.classList.add("location-opened");
-
-        window.open(
-            "https://www.google.com/maps?q=30.541998,31.693445",
-            "_blank"
-        );
-    });
+    window.open("https://www.google.com/maps?q=30.541998,31.693445", "_blank");
+  });
 }
 
-
 if (locationIcon2) {
-    locationIcon2.addEventListener("click", () => {
+  locationIcon2.addEventListener("click", () => {
+    sessionStorage.setItem("weddingOpened", "true");
+    locationIcon2.classList.add("location-opened");
 
-        sessionStorage.setItem("weddingOpened", "true");
-        locationIcon2.classList.add("location-opened");
-
-        window.open(
-            "https://www.google.com/maps?q=30.59697, 31.46964",
-            "_blank"
-        );
-    });
+    window.open("https://www.google.com/maps?q=30.600691,31.490609", "_blank");
+  });
 }
 
 const heart = document.querySelector(".heart-icon");
- if(heart){
- heart.addEventListener("click", () => {
+if (heart) {
+  heart.addEventListener("click", () => {
     if (heart.classList.contains("fa-regular")) {
-        heart.classList.remove("fa-regular");
-        heart.classList.add("fa-solid", "liked");
+      heart.classList.remove("fa-regular");
+      heart.classList.add("fa-solid", "liked");
     } else {
-        heart.classList.remove("fa-solid", "liked");
-        heart.classList.add("fa-regular");
+      heart.classList.remove("fa-solid", "liked");
+      heart.classList.add("fa-regular");
     }
- });
+  });
 }
 
-const photos = document.querySelectorAll('.photo');
+const photos = document.querySelectorAll(".photo");
 let activePhoto = null;
 
-photos.forEach(photo => {
-    photo.addEventListener('click', () => {
+photos.forEach((photo) => {
+  photo.addEventListener("click", () => {
+    if (photo === activePhoto) {
+      photos.forEach((img) => {
+        img.style.display = "block";
+        img.classList.remove("active");
+      });
 
-        if (photo === activePhoto) {
-            photos.forEach(img => {
-                img.style.display = 'block';
-                img.classList.remove('active');
-            });
+      activePhoto = null;
+    } else {
+      photos.forEach((img) => {
+        img.style.display = img === photo ? "block" : "none";
+        img.classList.remove("active");
+      });
 
-            activePhoto = null;
-        }
-
-        else {
-            photos.forEach(img => {
-                img.style.display = (img === photo) ? 'block' : 'none';
-                img.classList.remove('active');
-            });
-
-            photo.classList.add('active');
-            activePhoto = photo;
-        }
-    });
+      photo.classList.add("active");
+      activePhoto = photo;
+    }
+  });
 });
 
 const form = document.querySelector(".message-form");
@@ -144,45 +126,45 @@ const submitBtn = document.querySelector(".submit-btn");
 const STORAGE_KEY = "form_submitted";
 
 form.addEventListener("submit", async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (localStorage.getItem(STORAGE_KEY)) {
-        alert("This Device Send Comment Before");
-        return;
-    }
+  if (localStorage.getItem(STORAGE_KEY)) {
+    alert("This Device Send Comment Before");
+    return;
+  }
 
-    const username = document.getElementById("username").value;
-    const message = document.getElementById("user-message").value.replace(/\n/g, "\r\n" );
+  const username = document.getElementById("username").value;
+  const message = document
+    .getElementById("user-message")
+    .value.replace(/\n/g, "\r\n");
 
-    const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSdZYvD6iS8Fq477TywyeZVTCyQcsAPW9RMP9oVTmWDGoRvNgw/formResponse";
+  const formURL =
+    "https://docs.google.com/forms/d/e/1FAIpQLSdZYvD6iS8Fq477TywyeZVTCyQcsAPW9RMP9oVTmWDGoRvNgw/formResponse";
 
-    const formData = new FormData();
-    formData.append("entry.1656824463", username);
-    formData.append("entry.1158190294", message);
+  const formData = new FormData();
+  formData.append("entry.1656824463", username);
+  formData.append("entry.1158190294", message);
 
-    submitBtn.disabled = true;
+  submitBtn.disabled = true;
 
-    try {
-        await fetch(formURL, {
-            method: "POST",
-            mode: "no-cors",
-            body: formData
-        });
+  try {
+    await fetch(formURL, {
+      method: "POST",
+      mode: "no-cors",
+      body: formData,
+    });
 
-        localStorage.setItem(STORAGE_KEY, "true");
+    localStorage.setItem(STORAGE_KEY, "true");
 
-        form.reset();
+    form.reset();
+  } catch (error) {
+    console.error(error);
+    alert("error");
+  }
 
-    } catch (error) {
-        console.error(error);
-        alert("error");
-    }
-
-    
-    submitBtn.disabled = false;
+  submitBtn.disabled = false;
 });
 
 document.getElementById("whatsapp-icon").onclick = function () {
-    window.open("https://wa.me/201201114123", "_blank");
+  window.open("https://wa.me/201201114123", "_blank");
 };
-
